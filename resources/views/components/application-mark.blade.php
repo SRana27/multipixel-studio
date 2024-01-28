f988ccb25147cabffda98f4ec0ee76c40fd65500
SRana27 <sohel.ewu7@gmail.com>
2024-01-28 16:56:34 +0600

    blade title & title image

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,3 +1,3 @@
-<a href="/">
+<a href="">
     <img src="{{asset('/')}}website-assets/img/finallogo.png" alt="logo" height="100px" width="100px">
 </a>
--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,5 +1,5 @@
 <a href="/">
 
-    <img src="{{asset('/')}}website-assets/img/finallogo.png" alt="logo" height="100px" width="100px">
+    <img src="{{asset('/')}}website-assets/img/finallogo.png" alt="logo" height="120px" width="150px">
 
 </a>
